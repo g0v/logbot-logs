67a62cfb0726205e07de3378f7a6f8ac48744c8f
kiang <kiange@gmail.com>
2020-03-22 23:46:46 +0800

    script to filter specified author

---
+++ scripts/02_author.php
@@ -0,0 +1,21 @@
+<?php
+$basePath = dirname(__DIR__);
+$timeBegin = strtotime('2013-07-26');
+$timeEnd = time();
+$author = 'kiang';
+$authorLog = $basePath . '/logs/' . $author . '.csv';
+$fh = fopen($authorLog, 'w');
+fputcsv($fh, array('time', 'msg'));
+for($timeCurrent = $timeBegin; $timeCurrent < $timeEnd; $timeCurrent += 86400) {
+    $theDate = date('Y-m-d', $timeCurrent);
+    $yearPath = $basePath . '/logs/' . date('Y', $timeCurrent);
+    $targetFile = $yearPath . '/' . $theDate . '.json';
+    if(file_exists($targetFile)) {
+        $lines = json_decode(file_get_contents($targetFile), true);
+        foreach($lines AS $line) {
+            if($line['nick'] === $author) {
+                fputcsv($fh, array($line['time'], $line['msg']));
+            }
+        }
+    }
+}
